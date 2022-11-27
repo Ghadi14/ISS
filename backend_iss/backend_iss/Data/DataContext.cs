e30de0ccd594f821ec2a762f3506cd4b915ff7e1
Ghadi <ghadi@tekwin.digital>
2022-11-27 22:39:35 +0200

    first commit

---
+++ backend_iss/backend_iss/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using backend_iss.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_iss.Data
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
